2bc758a7c88334411a5efe87b3e2c525c0aced10
sara-poker <spoker1412@gmail.com>
2025-09-18 17:39:47 +0330

    add chart to province page

--- apps/report/views.py
+++ apps/report/views.py
@@ -158,12 +158,38 @@ class ProvinceView(TemplateView):
         province = self.kwargs["pk"]
         speed_test = getattr(self.request, "_speed_test", {})
 
-        speed_test_success = speed_test.filter(test_state=True).count()
-        speed_test_fail = speed_test.count() - speed_test_success
+        speed_test_success = speed_test.filter(test_state=True)
 
         name = PROVINCES_FA.get(province, province)
 
+        # دسته بندی اول: ISP
+        data = []
+        for isp_id, isp_name in (
+            speed_test_success
+                .values_list('network_info__isp', 'network_info__isp__name')
+                .distinct()
+        ):
+            qs = speed_test_success.filter(network_info__isp=isp_id)
+            total = qs.count()
+
+            def speed_range(min_, max_):
+                return qs.filter(speed_mbps__gte=min_, speed_mbps__lt=max_).count()
+
+            subdata = [
+                {"category": "خیلی سریع", "value": speed_range(75, 101)},
+                {"category": "سریع", "value": speed_range(50, 75)},
+                {"category": "متوسط", "value": speed_range(25, 50)},
+                {"category": "کم سرعت", "value": speed_range(0, 25)},
+            ]
+
+            data.append({
+                "category": isp_name or "نامشخص",  # اینجا نام واقعی ISP قرار می‌گیرد
+                "value": total,
+                "subData": subdata
+            })
+
         context["province"] = name
+        context["chart_data"] = data
         return context
 
 
